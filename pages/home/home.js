56c2d9896c5ccf5c39605de42e42a7964189f123
chenyongzhi <chenyongzhijjk@163.com>
2020-01-21 14:51:26 +0800

    how to start

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,31 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'cyz',
+    age: 18,
+    student: [
+      {id: 110, name: 'kobe', age: 30},
+      {id: 111, name: 'james', age: 28},
+      {id: 112, name: 'curry', age: 32},
+      {id: 113, name: 'cyz', age: 20}
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // 1.错误做法，界面不会刷新的
+    // console.log('xxx')
+    // this.data.counter++
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    // console.log('zzz')
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
