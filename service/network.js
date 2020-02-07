d4d02b27d0c0fe143db8e4b9fa648f427dfeeed3
chenyongzhi <chenyongzhijjk@163.com>
2020-02-07 14:35:13 +0800

    '封装网络请求'

---
+++ service/network.js
@@ -0,0 +1,27 @@
+// export default function request(options) {
+//   return new Promise((resolve, reject) => {
+//     wx.request({
+//       url: options.url,
+//       method: options.method || 'get',
+//       data: options.data || {},
+//       success: (res) => {
+//         resolve(res)
+//       },
+//       fail: (err) => {
+//         reject(err)
+//       }
+//     })
+//   })
+// }
+
+export default function request(options) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: options.url,
+      method: options.method || 'get',
+      data: options.data || {},
+      success: resolve,
+      fail: reject
+    })
+  })
+}
