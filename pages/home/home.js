160747bab4911352df8e2387e075aefe959090bc
chenyongzhi <chenyongzhijjk@163.com>
2020-02-07 15:25:36 +0800

    '弹窗、分享'

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,60 +7,73 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      success: function() {
+        console.log('展示弹窗成功');
+      },
+      fail: function() {
+        console.log('展示弹窗失败');
+      },
+      complete: function() {
+        console.log('完成函数的调用');
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowModel() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: (res) => {
+        console.log(res);
+        if (res.cancel) {
+          console.log("用户点击了取消");
+        }
+        if (res.confirm) {
+          console.log('用户点击了确定');
+        }
+      }
+    })
   },
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    setTimeout(() => {
+      wx.hideLoading()
+    }, 1000);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleShowAction() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: (res) => {
+        console.log(res);
+        // switch (res.tabIndex) {
+        //   case 0:
+            
+        //     break;
+        
+        //   default:
+        //     break;
+        // }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options) {
+    return {
+      title: '你好',
+      path: '/pages/about/about',
+      imageUrl: '' 
+    }
   }
 })
