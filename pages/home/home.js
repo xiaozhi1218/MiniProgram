d8d006eb8af7f81807801f6ba1d9035eb7f0f86c
chenyongzhi <chenyongzhijjk@163.com>
2020-02-08 21:56:21 +0800

    goods

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,10 @@
 // pages/home/home.js
 import {
-  getMultiData
-} from  '../../service/home'
+  getMultiData,
+  getGoodsData
+} from '../../service/home'
+
+const types = ['pop', 'new', 'sell']
 
 Page({
 
@@ -11,7 +14,22 @@ Page({
   data: {
     banners: [],
     recommends: [],
-    titles: ['流行', '新款', '精选']
+    titles: ['流行', '新款', '精选'],
+    goods: {
+      "pop": {
+        page: 0,
+        list: []
+      },
+      "new": {
+        page: 0,
+        list: []
+      },
+      "sell": {
+        page: 0,
+        list: []
+      }
+    },
+    currentType: 'pop'
   },
 
   /**
@@ -19,6 +37,15 @@ Page({
    */
   onLoad: function (options) {
     // 1.请求轮播图和推荐数据
+    this._getMultiData()
+
+    // 2.请求商品数据
+    this._getGoodsData('pop')
+    this._getGoodsData('new')
+    this._getGoodsData('sell')
+  },
+  // ---------网络请求相关-----------
+  _getMultiData() {
     getMultiData().then(res => {
       // console.log(res)
       // 取出轮播图以及推荐数据
@@ -32,11 +59,43 @@ Page({
       })
     })
   },
+  _getGoodsData(type) {
+    // 1.获取页码
+    const page = this.data.goods[type].page + 1
+
+    // 2.发送网络请求
+    getGoodsData(type, page).then(res => {
+      // 2.1 取出数据
+      const list = res.data.data.list
+
+      // 2.2 将数据保存到对应type的list中
+      // const oldList = this.data.goods[type].list
+      // oldList.push(...list)
+      const goods = this.data.goods
+      goods[type].list.push(...list)
+      goods[type].page += 1
+
+      // 2.3 将数据设置到data中的goods中
+      // const typeKey = 'goods.${type}.list'
+      // const pageKey = 'goods.${type}.page'
+
+      this.setData({
+        // [typeKey]: oldList,
+        // [pageKey]: page
+        goods: goods
+      })
+    })
+  },
+  // ------------事件监听相关--------------
   handleTabClick(event) {
     // console.log(event);
     // 取出index
     const index = event.detail.index
-    console.log(index);
+
+    // 2.设置currentType
+    this.setData({
+      currentType: types[index]
+    })
   },
 
   /**
