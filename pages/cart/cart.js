edc260b93fbf1ddf5ebfd3c4554ffa9fc24e8e0a
chenyongzhi <chenyongzhijjk@163.com>
2020-02-08 15:29:46 +0800

    '首页'

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,42 +1,18 @@
-// pages/home/home.js
-import {
-  getMultiData
-} from  '../../service/home'
-
+// pages/cart/cart.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    banners: [],
-    recommends: [],
-    titles: ['流行', '新款', '精选']
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // 1.请求轮播图和推荐数据
-    getMultiData().then(res => {
-      // console.log(res)
-      // 取出轮播图以及推荐数据
-      const banners = res.data.data.banner.list
-      const recommends = res.data.data.recommend.list
 
-      // 将banners和recommends放到data中
-      this.setData({
-        banners,
-        recommends
-      })
-    })
-  },
-  handleTabClick(event) {
-    // console.log(event);
-    // 取出index
-    const index = event.detail.index
-    console.log(index);
   },
 
   /**
